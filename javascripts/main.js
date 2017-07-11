c346e101415e3f4b60e320d46ea5b704611d8da2
Joshua Lloyd <joshualloyd@joshualloyd.com>
2017-07-11 14:14:38 -0500

    finally have the getter with the new array for attractions

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,26 +2,27 @@
 
 console.log('hello');
 
-let app = Object.create(null);
-
-let attractions = [];
-let areas = [];
-let attractionTypes = [];
+// let attractions = [];
+// let areas = [];
+// let attractionTypes = [];
 let attractionsList = [];
 
+let app = Object.create(null);
+
 let data = require('./data-factory');
-let attractionsListBuilder = require('./attractions-list-data-builder');
+let getAttractionsList = require('./attractions-list-data-builder');
 
-attractionsListBuilder();
+getAttractionsList().then(function(dataFromGetAttractionsList){
+	// console.log(dataFromGetAttractionsList);
+	attractionsList = dataFromGetAttractionsList;
+});
 
-// data.getAllAttractions()
-// 	.then(function(dataFromGetAllAttractions){
-// 		// console.log('data from getAllAttractions', dataFromGetAllAttractions);
-// 		attractions = dataFromGetAllAttractions;
-// 	});
-
-app.getAttractionsList = function(){
+app.listGetter = function(){
 	return attractionsList;
 };
+
+
+
+
 
 window.app = app;
