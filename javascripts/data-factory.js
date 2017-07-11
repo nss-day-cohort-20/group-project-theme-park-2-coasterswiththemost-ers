f7b45d13d9b29596b5d64b375342e610ab72649b
Joshua Lloyd <joshualloyd@joshualloyd.com>
2017-07-10 23:22:53 -0500

    initial work on xhr calls to firebase

---
+++ javascripts/data-factory.js
@@ -0,0 +1,46 @@
+'use strict';
+
+let $ = require('jquery');
+
+const dataFactory = Object.create(null);
+
+dataFactory.getAllAttractions = function() {
+  return new Promise( function(resolve, reject){
+    $.ajax({
+      url: "https://themepark-a3934.firebaseio.com/attractions.json"
+    })
+    .done(function(data) {
+    	console.log('attractions', data);
+      resolve(data);
+    })
+    .fail(reject);
+  });
+};
+
+dataFactory.getAllAreas = function() {
+	return new Promise( function(resolve, reject){
+    $.ajax({
+      url: "https://themepark-a3934.firebaseio.com/areas.json"
+    })
+    .done(function(data) {
+    	console.log('areas', data);
+      resolve(data);
+    })
+    .fail(reject);
+  });
+};
+
+dataFactory.getAllAttractionTypes = function() {
+	return new Promise( function(resolve, reject){
+    $.ajax({
+      url: "https://themepark-a3934.firebaseio.com/attraction_types.json"
+    })
+    .done(function(data) {
+    	console.log('attraction types', data);
+      resolve(data);
+    })
+    .fail(reject);
+  });
+};
+
+module.exports = dataFactory;
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
 console.log('hello');
+
+let data = require('./data-factory');
+
+let attractionsData = data.getAllAttractions();
+let areasData = data.getAllAreas();
+let attractionTypesData = data.getAllAttractionTypes();
+// console.log(attractionsData);
+// console.log(areasData);
+// console.log(attractionTypesData);
