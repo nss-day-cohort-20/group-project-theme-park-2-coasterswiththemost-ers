160e93374c5195fcd1b7a0508ddeceb5fa23565b
Joshua Lloyd <joshualloyd@joshualloyd.com>
2017-07-11 22:47:57 -0500

    got search by name to work, but need to be extrated into a module

--- javascripts/main.js
+++ javascripts/main.js
@@ -7,6 +7,8 @@ console.log('hello');
 // let attractionTypes = [];
 let attractionsList = [];
 
+let textInput = document.getElementById('text-input');
+
 let app = Object.create(null);
 let $ = require('jquery');
 let data = require('./data-factory');
@@ -14,7 +16,7 @@ let getAttractionsList = require('./attractions-list-data-builder');
 let templates = require('./templates');
 
 getAttractionsList().then(function(dataFromGetAttractionsList){
-	console.log(dataFromGetAttractionsList);
+	// console.log(dataFromGetAttractionsList);
 	attractionsList = dataFromGetAttractionsList;
 	$('#attractionList').append(templates.testTemplate({list : attractionsList}));
 });
@@ -22,11 +24,24 @@ getAttractionsList().then(function(dataFromGetAttractionsList){
 app.listGetter = function(){
 	return attractionsList;
 };
-let x = app.listGetter();
-console.log("x",x);
-// let list = ;
-console.log(attractionsList);
 
-
+textInput.addEventListener('keyup', function(){
+	if (event.key === 'Enter') {
+		let searchedAttractions = attractionsList.filter(function(attraction){
+			// console.log(attraction);
+			function stringContains(){
+				if (attraction.name.toLowerCase().search(textInput.value.toLowerCase()) === -1) {
+					return false;
+				} else {
+					return true;
+				}
+			}
+			return stringContains();
+		});
+		// console.log('searchedAttractions', searchedAttractions);
+		$('#attractionList').empty();
+		$('#attractionList').append(templates.testTemplate({list : searchedAttractions}));
+	}
+});
 
 window.app = app;
